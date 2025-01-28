08ce449cb45f0d3e4054b0882ca5e3a4685fa0d1
wi11-holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2025-01-28 19:09:19 +1100

    gitignore was ignoring files in blog/

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { 
+    nixpkgs, 
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
